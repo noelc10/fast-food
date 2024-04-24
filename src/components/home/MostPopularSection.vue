288f3ede12b54f6476db7c141751921b7d128d40
Noel Calonia <emonoel10@gmail.com>
2024-04-25 06:17:51 +0800

    [UI]: Product Card component and minor page tweak

--- src/components/home/MostPopularSection.vue
+++ src/components/home/MostPopularSection.vue
@@ -6,36 +6,19 @@
       </div>
     </div>
     <div class="flex flex-wrap justify-start most-popular-container">
-      <div
+      <product-card
         v-for="(product, i) in products"
         :key="i"
-        class="flex flex-col food-card"
-      >
-        <div class="food-card-img">
-          <img :alt="product.title" :src="product.img" />
-        </div>
-        <div class="food-card-container">
-          <div class="truncate ion-text-capitalize food-card-title">
-            {{ product.title }}
-          </div>
-          <div class="truncate ion-text-capitalize food-card-subtitle">
-            {{ product.type }}
-          </div>
-          <div class="mt-4 flex justify-between items-center food-card-details">
-            <div class="food-card-details-price">P {{ product.price }}</div>
-            <div class="food-card-details-rating">
-              <img alt="Rating" :src="'/src/assets/images/foods/rating.svg'" />
-            </div>
-          </div>
-        </div>
-      </div>
+        :product="product"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import { storeToRefs } from 'pinia';
-import { useProductStore } from '@/store/productStore.js'
+import { useProductStore } from '@/store/productStore.js';
+import ProductCard from '@/components/products/ProductCard.vue';
 
 const productStore = useProductStore();
 const { products } = storeToRefs(productStore);
---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="props.product" class="flex flex-col food-card">
+    <div class="food-card-img">
+      <img :alt="props.product.title" :src="props.product.img" />
+    </div>
+    <div class="food-card-container">
+      <div class="truncate ion-text-capitalize food-card-title">
+        {{ props.product.title }}
+      </div>
+      <div class="truncate ion-text-capitalize food-card-subtitle">
+        {{ props.product.type }}
+      </div>
+      <div class="mt-4 flex justify-between items-center food-card-details">
+        <div class="food-card-details-price">P {{ props.product.price }}</div>
+        <div class="food-card-details-rating">
+          <img alt="Rating" :src="'/src/assets/images/foods/rating.svg'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  product: {
+    type: Object,
+    default: () => { return null }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
